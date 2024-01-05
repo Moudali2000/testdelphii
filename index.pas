3ec3227271e7bbb4e285e5da3da17171b1f76487
Moudali2000 <155449541+Moudali2000@users.noreply.github.com>
2024-01-05 21:01:00 +0100

    Add files via upload

---
+++ index.pas
@@ -0,0 +1,38 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, JS, Web, WEBLib.Graphics, WEBLib.Controls,
+  WEBLib.Forms, WEBLib.Dialogs, Vcl.Controls, Vcl.StdCtrls, WEBLib.StdCtrls;
+
+type
+  TForm1 = class(TWebForm)
+    WebButton1: TWebButton;
+    WebButton2: TWebButton;
+    WebLabel1: TWebLabel;
+    WebLabel2: TWebLabel;
+    WebLabel3: TWebLabel;
+    WebLabel4: TWebLabel;
+    WebLabel5: TWebLabel;
+    WebLabel6: TWebLabel;
+    procedure WebButton1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.WebButton1Click(Sender: TObject);
+begin
+ShowMessage('TBY61zM6HX7hC2kdGyrEmD6XiRv1DoJiiT');
+end;
+
+end.
